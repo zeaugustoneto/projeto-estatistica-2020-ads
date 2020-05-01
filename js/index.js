4d31db4dfc22ded8c2f9e42997abd240408de1ce
José Augusto Neto <netiin00@gmail.com>
2020-05-01 06:35:32 -0300

    adicionei o qualitativo

--- js/index.js
+++ js/index.js
@@ -3,7 +3,36 @@ document.querySelector("#add-data").addEventListener("click", function() {
   let varName = document.getElementById("var-name").value;
   let input = document.getElementById("dataInput").value;
 
-  if (typeOfData.value === "quantitativa") {
+
+
+  /*function criarCabecalho(name){
+  let cabecalho = document.getElementById('cabecalho')
+
+  let titulo = document.createElement("th");
+  let freqSimples = document.createElement("th")
+  let freqSimples_porcento = document.createElement("th")
+  let freqAcm = document.createElement("th")
+  let freqAcm_porcento = document.createElement("th")
+  
+
+
+
+  
+  cabecalho.appendChild(titulo);
+  cabecalho.appendChild(freqSimples);
+  cabecalho.appendChild(freqSimples_porcento);
+  cabecalho.appendChild(freqAcm);
+  cabecalho.appendChild(freqAcm_porcento);
+
+  titulo.innerText = name
+  freqSimples.innerText = 'Frequência Simples'
+  freqSimples_porcento.innerText = 'Frequência Simples %'
+  freqAcm.innerText = 'Frequência Acumulada'
+  freqAcm_porcento.innerText = 'Frequência Acumulada %'
+  }
+*/
+
+  if (typeOfData.value === "quantitativa") { //selecionador de quantitativa ou qualitativa
     quantitativeFunc(varName, input);
   } else {
     qualitativeFunc(varName, input);
@@ -13,14 +42,20 @@ let newArray;
 
 //testes 25;98;54;12;0;1;2;6;4;71;5;30;20;84;4    1;2;2;5;4;1;2;6;4;1;2
 
-function quantitativeFunc(name, array) {
-  newArray = array.split(";").map(x => parseFloat(x));
 
-  //   let sortArray = selectionSort(newArray);
-  let sortArray = newArray.sort((a, b) => a - b);
-  let freqArray = [], facArray = []
+function qualitativeFunc(name, array){
+
+  //testes branco;azul;preto;preto;branco;branco;negro;azul;amarelo;yellow
+  document.getElementById('headVar').innerText = name
+  newArray = array.split(";")
+
+  let sortArray = newArray.sort() 
+
+  console.log(sortArray)
+
+  let freqArray = [], facArray = [] 
   let x, fac = 0
-  for (let i = 0; i < sortArray.length; i = x) {
+  for (let i = 0; i < sortArray.length; i = x) {  //função que compara um item do vetor com outro e caso for true, adiciona um contador para fazer frequencia normal
     let freqCont = 1;
 
     for (x = i + 1; x < sortArray.length; x++) {
@@ -30,22 +65,64 @@ function quantitativeFunc(name, array) {
         break;
       }
     }
-    fac += freqCont
-    facArray.push(fac)
-    freqArray.push(freqCont);
+    fac += freqCont // acumulador para frequencia acumulada
+    facArray.push(fac) // push no array de frequencia acumulada
+    freqArray.push(freqCont); // push no array de frequencia normal
     
     // console.log(`Elemento = ${sortArray[i]}; frequencia = ${freqCont}`);
   }
 
-  let filteredArray = sortArray.filter((este, i) => sortArray.indexOf(este) === i);
-  console.log(sortArray);
-  console.log(freqArray);
+
+
+let filteredArray = sortArray.filter((este, i) => sortArray.indexOf(este) === i)
+console.log(filteredArray)
+
+writeTable(filteredArray, freqArray,facArray, sortArray) // chamada da função de escrever tabela
+
+
+
+}
+
+
+
+
+
+
+
+
+function quantitativeFunc(name, array) {  //falta adicionar o nome, mas isso é o de menos
+  document.getElementById('headVar').innerText = name
+  newArray = array.split(";").map(x => parseFloat(x));  //separa o array por ";" e com map() passa tudo para Number float
+
+  let sortArray = newArray.sort((a, b) => a - b); //function que o Glenio me passou para ordernar o vetor por ordem crescente
+  let freqArray = [], facArray = [] 
+  let x, fac = 0
+  for (let i = 0; i < sortArray.length; i = x) {  //função que compara um item do vetor com outro e caso for true, adiciona um contador para fazer frequencia normal
+    let freqCont = 1;
+
+    for (x = i + 1; x < sortArray.length; x++) {
+      if (sortArray[i] == sortArray[x]) {
+        freqCont++;
+      } else {
+        break;
+      }
+    }
+    fac += freqCont // acumulador para frequencia acumulada
+    facArray.push(fac) // push no array de frequencia acumulada
+    freqArray.push(freqCont); // push no array de frequencia normal
+    
+    // console.log(`Elemento = ${sortArray[i]}; frequencia = ${freqCont}`);
+  }
+
+  let filteredArray = sortArray.filter((este, i) => sortArray.indexOf(este) === i); // função que filtra o vetor para tirar todos números repetidos
+  /*console.log(sortArray);
+  console.log(freqArray);*/
 
  //let freqTotal = accumulate(freqArray)
 
 
 
-writeTable(filteredArray, freqArray,facArray, sortArray)
+writeTable(filteredArray, freqArray,facArray, sortArray) // chamada da função de escrever tabela
 
   }
 
@@ -66,14 +143,14 @@ writeTable(filteredArray, freqArray,facArray, sortArray)
 
 // teste para git
 
-function writeTable(element, freq , fac, array) {
+function writeTable(element, freq , fac, array) { //função para escrever na tabela
 
 
-  function freqIpercent(repetition , array){
+  function freqIpercent(repetition , array){ // função de frequencia normal porcento
     let aux = repetition / array.length
     let percentual = aux * 100
     freqIpercentArray.push(percentual.toFixed(0))
-    freqIpercentArray = freqIpercentArray.map(x => parseFloat(x))
+    freqIpercentArray = freqIpercentArray.map(x => parseFloat(x)) // conversão para number
    // return percentualFixed
 
   }  
@@ -81,29 +158,32 @@ function writeTable(element, freq , fac, array) {
  
 
 
-
-  for (let i = 0; i < element.length; i++) {
-    let linha = document.createElement("tr");
+  for (let i = 0; i < element.length; i++) { //escreve a tabela
+    var linha = document.createElement("tr");
     document.getElementById("col").appendChild(linha)
-    let coluna1 = document.createElement("td");
-    let coluna2 = document.createElement("td")
-    let coluna3 = document.createElement("td")
-    let coluna4 = document.createElement("td")
-    let coluna5 = document.createElement("td")
+    var coluna1 = document.createElement("td");
+    var coluna2 = document.createElement("td")
+    var coluna3 = document.createElement("td")
+    var coluna4 = document.createElement("td")
+    var coluna5 = document.createElement("td")
 
+
+  
+ 
     
-
     linha.appendChild(coluna1);
     linha.appendChild(coluna2);
     linha.appendChild(coluna3);
     linha.appendChild(coluna4);
     linha.appendChild(coluna5);
+    
+
 
     freqIpercent(freq[i], array)
 
     console.log(freqIpercentArray)
  
-     acmFreq += Math.floor(freqIpercentArray[i])
+     acmFreq += Math.floor(freqIpercentArray[i]) // frequencia acumulada porcento
      
 
 
@@ -112,13 +192,18 @@ function writeTable(element, freq , fac, array) {
     coluna3.innerText = freqIpercentArray[i]
     coluna4.innerText = fac[i]
     coluna5.innerText = acmFreq
+    
     //let coluna2 = document.getElementsByClassName('coluna2')
     //coluna1.innerT = element
 
 
   }
 
-  
+    /*linha.removeChild(coluna1);
+    linha.removeChild(coluna2);
+    linha.removeChild(coluna3);
+    linha.removeChild(coluna4);
+    linha.removeChild(coluna5);*/
 
  /* celula.innerText = element;
   celula2.innerText = freq;*/
